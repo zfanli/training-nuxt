d33d2e6548008e15795a2ef436daf134354891c9
Richard <cyanyao@live.com>
2019-04-14 14:49:52 +0800

    revise css

--- components/BlogHeader.vue
+++ components/BlogHeader.vue
@@ -16,7 +16,13 @@
   top: 0;
   background-color: rgba(255, 255, 255, 0.6);
   width: 100vw;
-  box-shadow: 0px 3px 13px -5px rgba(0, 0, 0, 0.4);
+  box-shadow: 0px 3px 5px -5px rgba(0, 0, 0, 0.4);
+  transition: all 0.2s ease;
+
+  &:hover {
+    box-shadow: 0px 3px 26px -5px rgba(0, 0, 0, 0.4);
+    transform: translate(1px, -1px);
+  }
 
   .header-home,
   .header-links {
@@ -25,7 +31,14 @@
     margin: 1rem;
 
     a {
+      display: block;
       color: #333;
+      transition: all 0.2s ease;
+
+      &:hover {
+        transform: translate(1px, -1px);
+      }
+      // Todo text shadow?
     }
   }
 
