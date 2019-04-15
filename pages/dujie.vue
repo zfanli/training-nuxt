6fd49c7b3482be07465339bdeee086f6bcf9ba59
Richard <cyanyao@live.com>
2019-04-15 20:57:16 +0800

    add unique prefix

--- pages/dujie.vue
+++ pages/dujie.vue
@@ -1,19 +1,19 @@
 <template>
-  <section class="container">
+  <section class="dujie-container">
     <div>
-      <h1 class="title">渡劫</h1>
-      <h2 class="subtitle">何方道友在此渡劫？</h2>
+      <h1 class="dujie-title">渡劫</h1>
+      <h2 class="dujie-subtitle">何方道友在此渡劫？</h2>
     </div>
   </section>
 </template>
 
 <style>
-.container {
+.dujie-container {
   display: flex;
   justify-content: center;
   text-align: center;
 }
-.title {
+.dujie-title {
   font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
     'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
   display: block;
@@ -24,7 +24,7 @@
   text-shadow: 4px 4px 0 #333, -2px -2px 0 #333, 4px -2px 0 #333,
     -2px 4px 0 #333, -1px 3px 0 #333;
 }
-.subtitle {
+.dujie-subtitle {
   font-weight: 300;
   font-size: 42px;
   color: #526488;
