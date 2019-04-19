7aedf51949841dcd5098b1d1a4138a46cbffefc8
Richard <cyanyao@live.com>
2019-04-19 14:58:47 +0800

    test dynamic route

---
+++ pages/testDynamicRoute/_id.vue
@@ -0,0 +1,23 @@
+<template>
+  <article>{{ post.title }}</article>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+
+export default {
+  layout: 'default',
+  transition: 'opacity',
+  computed: {
+    ...mapState(['list']),
+    post() {
+      for (const p of this.list) {
+        if (p.path === this.$route.params.id) {
+          return p
+        }
+      }
+      return { title: 'No Found' }
+    }
+  }
+}
+</script>
